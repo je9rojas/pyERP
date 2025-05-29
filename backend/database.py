51710b91a47bf3a32571244de2be610c0f0a7489
je9rojas <je9rojas@gmail.com>
2025-05-29 17:36:18 -0500

    v1.0

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,14 @@
+# backend/database.py
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+# Asegúrate de que esta clave coincida con la definida en render.yaml
+MONGO_URI = os.getenv("MONGODB_URI")
+
+# Conexión con MongoDB
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["erp_db"]
